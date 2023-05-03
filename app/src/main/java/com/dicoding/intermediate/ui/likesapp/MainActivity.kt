a9d34ab7cf4f4e438b69ed7f0e02ee211b7b0cf2
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-04 06:47:14 +0700

    drawing face object to canvas

--- app/src/main/java/com/dicoding/intermediate/ui/likesapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/intermediate/ui/likesapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.dicoding.intermediate.ui.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.TypefaceCompat.ResourcesCallbackAdapter
+import com.dicoding.intermediate.ui.likesapp.databinding.ActivityMainBinding
+import java.util.ResourceBundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.setImageBitmap(mBitmap)
+
+        showFace()
     }
+
+    private val halfOfWidth = (mBitmap.width / 2).toFloat()
+    private val halfOfHeight = (mBitmap.height / 2).toFloat()
+
+    private val left = 150f
+    private val top = 250f
+    private val right = mBitmap.width - left
+    private val bottom = mBitmap.height.toFloat() - 50f
+
+    private fun showFace() {
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90f, 180f, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
+        mCanvas.drawArc(face, 270f, 180f, false, mPaint)
+    }
+
 }
